79c9db684be53409aadec0337d94771619d5d007
SKLC1 <segevklein2000@gmail.com>
2022-05-10 19:02:38 +0300

    all api mini exrecises

---
+++ API mini-exrecises/movie-api/app.js
@@ -0,0 +1,59 @@
+const btn = document.querySelector('.btn')
+const searchbar = document.querySelector('.searchbar')
+
+btn.addEventListener('click', ()=>{
+   getMovieData(searchbar.value)
+})
+
+async function getMovieData(movieName) {
+  const res = await fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=a4a2c31b`)
+  const data = await res.json()
+  try{
+    const movieData = {
+      poster: data.Poster,
+      title: data.Title,
+      genre: data.Genre,
+      year: data.Year,
+      plot: data.Plot,
+      director: data.Director,
+      actors: data.Actors,
+      ratingsArr: data.Ratings,
+    }
+    console.log(movieData);
+    (movieData.title!==undefined)?createCard(movieData):alert('not found');
+    } catch {
+    console.log('error n');
+  }
+}
+
+function createCard(movieData) {
+  const cardContainer = document.querySelector('.card-container')
+  const card = document.createElement('div')
+  card.classList.add('card')
+  cardContainer.appendChild(card)
+  createCardContent(movieData,card)
+}
+function createCardContent(movieData,card) { 
+  console.log(movieData);
+  for (const key in movieData) {
+    if (key == 'poster') {
+      const stat = document.createElement('div')
+      stat.classList.add('poster')
+      stat.style.backgroundImage = `url('${movieData[key]}')`
+      card.appendChild(stat)
+    } else if (key == 'ratingsArr') {
+      console.log(movieData[key]);
+      movieData[key].forEach(e => {
+        const stat = document.createElement('div')
+        stat.innerText = `${e.Source}: ${e.Value}`
+        card.appendChild(stat)
+         });
+    } else {
+      const stat = document.createElement('div')
+      stat.innerText = `${key}: ${movieData[key]}`
+      card.appendChild(stat)
+    }
+    
+  }
+}
+
