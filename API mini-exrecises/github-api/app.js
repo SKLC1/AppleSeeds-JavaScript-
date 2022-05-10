72741487ff24f7a456c0ea9fff715f92171bcab2
SKLC1 <segevklein2000@gmail.com>
2022-05-10 15:00:47 +0300

    m

--- API mini-exrecises/github-api/app.js
+++ API mini-exrecises/github-api/app.js
@@ -7,34 +7,39 @@ btn.addEventListener('click', getUser)
 async function getUser() {// change username to input.value
   const userInput = input.value
   try {
-    const url =  `https://api.github.com/users/${userInput}`;
-    const res = await fetch(url);
-    const data = await res.json();
+    const url = `https://api.github.com/users/${userInput}`;
+    const res = await fetch(url)
+    const data = await res.json()
     if (data.message) {
       throw 'error'
     }
     const profilePic = data.avatar_url
     const name = data.login
     const reposNumber = data.public_repos
-    appendUser(profilePic,name,reposNumber)
+    const userLink = data.url
+    console.log(userLink);
+    appendUser(profilePic,userLink,name,reposNumber)
   } catch {
     console.log('error');
   }
 }
 
-const appendUser = (picUrl,name,repos) => {
+const appendUser = (picUrl,userLink,name,repos) => {
   const userCard = document.createElement('div')
   userCard.classList.add('card')
   usersContainer.appendChild(userCard)
-  createImage(picUrl,userCard)
+  createImage(picUrl,userLink,userCard)
   createName(name,userCard)
   createRepos(repos,userCard)
 }
-const createImage = (picUrl,userCard) => {
+const createImage = (picUrl,userLink,userCard) => {
   const image = document.createElement('div')
   image.classList.add('image')
   image.style.backgroundImage = `url('${picUrl}')`
   userCard.appendChild(image)
+  image.addEventListener('click', ()=>{
+    console.log(userLink);
+  })
 }
 const createName = (name,userCard) => {
   const username = document.createElement('div')
